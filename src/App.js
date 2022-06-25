c51935af698a94889b97408c8450c4b6f1108ca3
Sito8943 <sito8943@gmail.com>
2022-06-24 21:58:09 -0400

    working on styles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,123 @@
-import logo from './logo.svg';
-import './App.css';
+import { forwardRef } from "react";
 
-function App() {
+// @emotion
+import { css } from "@emotion/css";
+
+// sito components
+import SitoContainer from "sito-container";
+
+// prop-types
+import PropTypes from "prop-types";
+
+const SitoModal = forwardRef((props, ref) => {
+  const {
+    backdropBackground,
+    backdropFilter,
+    background,
+    extraProps,
+    children,
+    display,
+    alignItems,
+    justifyContent,
+    flexDirection,
+    className,
+    sx,
+    id,
+    name,
+    style,
+  } = props;
+
+  const modalSx = css({
+    flexDirection,
+    display,
+    alignItems,
+    justifyContent,
+    background: background || "transparent",
+    padding: "20px",
+    borderRadius: "1rem",
+    width: "250px",
+    height: "250px",
+    ...sx,
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <SitoContainer
+      sx={{
+        position: "absolute",
+        top: 0,
+        left: 0,
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        background: backdropBackground,
+        backdropFilter: backdropFilter,
+      }}
+    >
+      <SitoContainer
+        ref={ref}
+        style={style}
+        id={id}
+        name={name}
+        className={`${className} ${modalSx}`}
+        {...extraProps}
+      >
+        {children}
+      </SitoContainer>
+    </SitoContainer>
   );
-}
+});
 
-export default App;
+SitoModal.defaultProps = {
+  backdropBackground: "#222222ec",
+  backdropFilter: "blur(4px)",
+  background: "#222333",
+  alignItems: "left",
+  justifyContent: "left",
+  className: "",
+  id: "",
+  name: "",
+  sx: {},
+  style: {},
+  extraProps: {},
+  children: <span>This is a container</span>,
+};
+
+SitoModal.propTypes = {
+  children: PropTypes.node,
+  backdropBackground: PropTypes.string,
+  backdropFilter: PropTypes.string,
+  background: PropTypes.string,
+  className: PropTypes.string,
+  alignItems: PropTypes.string,
+  justifyContent: PropTypes.string,
+  id: PropTypes.string,
+  name: PropTypes.string,
+  /**
+   * The system prop that allows defining system overrides as well as additional CSS styles.
+   */
+  sx: PropTypes.oneOfType([
+    PropTypes.arrayOf(
+      PropTypes.oneOfType([PropTypes.func, PropTypes.object, PropTypes.bool])
+    ),
+    PropTypes.func,
+    PropTypes.object,
+  ]),
+  style: PropTypes.oneOfType([
+    PropTypes.arrayOf(
+      PropTypes.oneOfType([PropTypes.func, PropTypes.object, PropTypes.bool])
+    ),
+    PropTypes.func,
+    PropTypes.object,
+  ]),
+  extraProps: PropTypes.oneOfType([
+    PropTypes.arrayOf(
+      PropTypes.oneOfType([PropTypes.func, PropTypes.object, PropTypes.bool])
+    ),
+    PropTypes.func,
+    PropTypes.object,
+  ]),
+};
+
+export default SitoModal;
